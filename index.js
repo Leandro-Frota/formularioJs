5058a954693f42a484a22747e7ae27aceb4af1d8
Leandro-Frota <leandromfrota@gmail.com>
2023-12-06 16:59:09 -0300

    Estilizei o formulário e Adicionei lógica

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
-let nome= "Leandro";
-let idade= 14;
-let acompanhadaPelosPais = true;
-let temIngresso=true
-let testouPositivoParaCovid=false;
+// let nome= "José";
+// let idade= 14;
+// let acompanhadaPelosPais = true;
+// let temIngresso=true
+// let testouPositivoParaCovid=false;
 
 
-if((idade>=18 || acompanhadaPelosPais) && temIngresso && !testouPositivoParaCovid){
-    console.info("Pode entrar")
-}else{
-    console.info("Não pode entrar")
-}
+// if((idade>=18 || acompanhadaPelosPais) && temIngresso && !testouPositivoParaCovid){
+//     console.info("Pode entrar")
+// }else{
+//     console.info("Não pode entrar")
+// }
 
 
-const frm = document.querySelector("form");
-const resp = document.querySelector("p")
 
-frm.addEventListener("buttonSubmit",(e)=>{
-    const age = Number(frm.age.value);
-    const 
-})
+const btn = document.querySelector("#send")
+const resp = document.querySelector("#textReturn");
 
-console.info(resp)
+
+ btn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    const name=document.querySelector("#inName");
+    const age=document.querySelector("#inAge");
+    const compParents= document.querySelector("#checkParents");
+    const ticket=document.querySelector("#checkTicket");
+    const testCovid= document.querySelector("#checkTestCovid")
+
+    const valueName=name.value;
+    const valueAge=age.value;
+    const valueParents= compParents.checked;
+    const valueTicket=ticket.checked;
+    const valueTestCovid= testCovid.checked;
+
+    if((valueAge>=18 || valueParents ) && valueTicket && !valueTestCovid ){
+        console.log("Pode Entrar");
+        resp.innerText = "Pode Entrar!  "
+        
+    }else{
+        console.log("Não pode entrar!");
+        resp.innerText = "Não Pode Entrar!"
+    }
+
+
+
+    console.log(valueAge);
+    console.log(valueName);
+    console.log(valueParents);
+    console.log(valueTicket);
+    console.log(valueTestCovid);
+    
+
+   
+ })
+
+
+
+
+
